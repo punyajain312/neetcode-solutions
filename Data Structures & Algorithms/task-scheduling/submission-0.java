b74411a52c8617738e2069b5874fc5e71519cc71
punyajain312 <129947153+punyajain312@users.noreply.github.com>
2026-06-04 22:50:39 +0530

    Add: task-scheduling - submission-0

---
+++ Data Structures & Algorithms/task-scheduling/submission-0.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] count = new int[26];
+        for(char c : tasks){
+            count[c - 'A']++;
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int cnt : count){
+            if(cnt > 0) pq.add(cnt);
+        }
+
+        Queue<int[]> q = new LinkedList<>();
+        int time = 0;
+
+        while(!pq.isEmpty() || !q.isEmpty()){
+            time++;
+
+            if(pq.isEmpty()){
+                time = q.peek()[1];
+            }
+            else{
+                int cnt = pq.poll() - 1;
+                if(cnt > 0) q.add(new int[] {cnt, time + n});
+            }
+
+            if(!q.isEmpty() && q.peek()[1] == time){
+                pq.add(q.poll()[0]);
+            }
+        }
+
+        return time;
+    }
+}
